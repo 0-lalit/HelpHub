b51f30c6c5e5bb35df784c2d82d02c4b23ad40f5
Lalit Kumar <lalitkumar9684@gmail.com>
2025-04-18 11:57:23 -0500

    implementing database

--- app/src/main/java/com/example/helphub/GaTaItem.java
+++ app/src/main/java/com/example/helphub/GaTaItem.java
@@ -4,10 +4,14 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class GaTaItem implements Parcelable {
+    private String id;
     private String name;
     private String course;
     private boolean available;
     private String profileImageUrl;
+
+    // Default constructor required for Firebase
+    public GaTaItem() {}
 
     public GaTaItem(String name, String course, boolean available, String profileImageUrl) {
         this.name = name;
@@ -17,6 +21,7 @@ public class GaTaItem implements Parcelable {
     }
 
     protected GaTaItem(Parcel in) {
+        id = in.readString();
         name = in.readString();
         course = in.readString();
         available = in.readByte() != 0;
@@ -35,20 +40,44 @@ public class GaTaItem implements Parcelable {
         }
     };
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getCourse() {
         return course;
     }
 
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
     public boolean isAvailable() {
         return available;
     }
 
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
     public String getProfileImageUrl() {
         return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
     }
 
     @Override
@@ -58,6 +87,7 @@ public class GaTaItem implements Parcelable {
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
         dest.writeString(name);
         dest.writeString(course);
         dest.writeByte((byte) (available ? 1 : 0));
---
+++ app/src/main/java/com/example/helphub/MyfirebaseInstanceIdService.java
@@ -0,0 +1,35 @@
+package com.example.helphub;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyfirebaseInstanceIdService extends FirebaseMessagingService {
+    private static final String TAG = "MyFirebaseMsgService";
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        //sendRegistrationToServer(token);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Handle FCM messages here.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+}
